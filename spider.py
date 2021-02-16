5f3e23f090e16ec10eff892fe6523057fe60bcf0
Shahtr1 <tramboos123@gmail.com>
2021-02-16 20:56:07 +0530

    completed

--- spider.py
+++ spider.py
@@ -1,23 +1,28 @@
 import requests
 import re
-import urlparse
+# import urlparse # python 2
+import urllib.parse as urlparse
 
-
-target_url = "https://zsecurity.org"
+target_url = "http://10.0.2.9/mutillidae/"
 target_links = []
 
 def extract_links_from(url):
-    response = requests.get(target_url)
-    return re.findall('(?:href=")(.*?)"', response.content)
+    response = requests.get(url)
+    # return re.findall('(?:href=")(.*?)"', response.content) # python 2
+    return re.findall('(?:href=")(.*?)"', response.content.decode(errors="ignore")) # python 3
 
-href_links = extract_links_from(target_url)
+def crawl(url):
+    href_links = extract_links_from(url)
 
-for link in href_links:
-    link = urlparse.urljoin(target_url, link)
+    for link in href_links:
+        link = urlparse.urljoin(url, link)
 
-    if "#" in link:
-        link = link.split("#")[0]
+        if "#" in link:
+            link = link.split("#")[0]
 
-    if target_url in link and link not in target_links:
-        target_links.append(link)
-        print(link)
+        if target_url in link and link not in target_links:
+            target_links.append(link)
+            print(link)
+            crawl(link)
+
+crawl(target_url)
